236371fdc4790ea13fbb7dd6ac335bacd0c074c7
Doido <mailson.rocha92@gmail.com>
2026-01-08 13:14:49 -0300

    Seller Service and FindAll

---
+++ WebSalesAgain/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSalesAgain.Data;
+using WebSalesAgain.Models;
+
+namespace WebSalesAgain.Services
+{
+    public class SellerService
+    {
+        private readonly WebSalesAgainContext _context;
+
+        public SellerService(WebSalesAgainContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+    }
+}
--- WebSalesAgain/Startup.cs
+++ WebSalesAgain/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebSalesAgain.Data;
+using WebSalesAgain.Services;
 
 namespace WebSalesAgain
 {
@@ -41,6 +42,7 @@ namespace WebSalesAgain
                         builder.MigrationsAssembly("WebSalesAgain")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
