79d8b5f1722a010248db633865fd7199a3f32744
Doido <mailson.rocha92@gmail.com>
2026-01-08 11:02:56 -0300

    Remake Department with CRUD scaffolding

---
+++ WebSalesAgain/Data/WebSalesAgainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesAgain.Models;
+
+namespace WebSalesAgain.Data
+{
+    public class WebSalesAgainContext : DbContext
+    {
+        public WebSalesAgainContext (DbContextOptions<WebSalesAgainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSalesAgain.Models.Department> Department { get; set; }
+    }
+}
--- WebSalesAgain/Startup.cs
+++ WebSalesAgain/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesAgain.Data;
 
 namespace WebSalesAgain
 {
@@ -33,6 +35,9 @@ namespace WebSalesAgain
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebSalesAgainContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesAgainContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
