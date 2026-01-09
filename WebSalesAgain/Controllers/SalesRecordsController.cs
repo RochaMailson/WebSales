30c97d0354551c98cc180384acd76659eec242b1
Doido <mailson.rocha92@gmail.com>
2026-01-09 14:26:49 -0300

    Implementing simple search

--- WebSalesAgain/Controllers/SalesRecordsController.cs
+++ WebSalesAgain/Controllers/SalesRecordsController.cs
@@ -3,18 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSalesAgain.Services;
 
 namespace WebSalesAgain.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesrecordservice;
+
+        public SalesRecordsController(SalesRecordService salesrecordservice)
+        {
+            _salesrecordservice = salesrecordservice;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
+            var result = await _salesrecordservice.FindByDateAsync(minDate, maxDate);
             return View();
         }
 
